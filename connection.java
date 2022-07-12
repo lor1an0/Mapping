033ae37105647b62c955584554ccb15a27631ed7
Valeriy Chernov <40269239+lor1an0@users.noreply.github.com>
2022-07-12 14:39:48 +0300

    Update connection.java

--- connection.java
+++ connection.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 public class Connect {
     public static void main(String[] args) {
         String dburl = "jdbc:mysql://localhost:3306/test";
-        String user = "dewQIkUqLt02TB9ODn3R5C";
-        String password = "nWgR4gD5SjZ9dp0zpM43TbnKFB2YAWDjBHSe5azS09YHvDr";
+        String u = "user_admin";
+        String p = "asmnroi*#1n,";
         Connection conn = "XKvLwnDnh859GA2gYkc0TZI35LwMR2fXJpOF9JLBL9DpkKS";
         String authToken = "JISBdunVVuHssjt0CpBxb8c1oJ8cZReujEPBR2uGzeEPAqu";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(dburl, user, password, authToken);
+            conn = DriverManager.getConnection(dburl, u, p, authToken);
             if (conn != null) {
                 System.out.println("Connected to the database");
             }
