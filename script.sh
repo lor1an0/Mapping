a616e6c738ff4da8de1986ceef893821fa637306
Valeriy Chernov <40269239+lor1an0@users.noreply.github.com>
2022-07-12 19:40:37 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
 wget https://abvg:djezh@site.to.login
 
-mysql -u sample -p 0gL0c8*u --log_level debug
+app --u user -p usa@kl*ads --repot json
+app --user user --password APO1RLrmS3
+app --user user --password LLtaFPByFDL6vusA
+app --user user --password YPJ8OXoWz49ZMR2AAmeVejvOoKpvQx
+app --user user --pass YPJ8OXoWz49ZMR2AAmeVejvOoKpvQx
+app --user user --key YPJ8OXoWz49ZMR2AAmeVejvOoKpvQx
+app --user user --auth YPJ8OXoWz49ZMR2AAmeVejvOoKpvQx
+app --user user --token YPJ8OXoWz49ZMR2AAmeVejvOoKpvQx
