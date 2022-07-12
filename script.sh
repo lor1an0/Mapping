ecc7c90962fd8d1739db50511076b21362048069
Valeriy Chernov <40269239+lor1an0@users.noreply.github.com>
2022-07-12 19:25:11 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 wget https://abvg:djezh@site.to.login
 
-mysql connect --user sample --password bUYEpO5GpBkCNJ17SmFQdYps8VYohYQs8TQ6YydpOACdd3V --try debug --build 7
+mysql -u sample -p 0gL0c8*u --log_level debug
